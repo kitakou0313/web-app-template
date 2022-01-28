bbcbe801ba7d50d9e243831c6a4f43881cd1f265
kitakou0313 <kitakou0313@gmail.com>
2022-01-28 15:01:03 +0900

    Define test web server

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,9 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func hello(c echo.Context) error {
+	return c.String(
+		http.StatusOK, "Hello, World! 2",
+	)
+}
 
 func main() {
 
-	testString := "Reflex hot loading test 2"
-	fmt.Println(testString)
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", hello)
+
+	e.Logger.Fatal(e.Start(":80"))
 }
